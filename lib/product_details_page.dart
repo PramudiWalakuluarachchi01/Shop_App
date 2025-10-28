df96da887a5c1753cab575ff680fc3c6a83d0ba5
PramudiWalakuluarachchi01 <pramudinethmini01@gmail.com>
2025-10-28 23:40:21 +0530

    `Added product details page and bottom navigation bar to home page`

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -9,84 +9,91 @@ class ProductDetailsPage extends StatefulWidget {
 }
 
 class _ProductDetailsPageState extends State<ProductDetailsPage> {
-
- int selectedSize = 0;
-
+  int selectedSize = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Details')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            widget.product['title'] as String,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset(widget.product['imageUrl'] as String),
-          ),
-          const Spacer(flex: 2),
-          Container(
-            height: 250,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: const Color.fromRGBO(245, 247, 249, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.product['title'] as String,
+              style: Theme.of(context).textTheme.titleLarge,
             ),
-            child: Column(
-              children: [
-                Text(
-                  '\$${widget.product['price']}',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 50,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: (widget.product['sizes'] as List<int>).length,
-                    itemBuilder: (context, index) {
-                      final size =
-                          (widget.product['sizes'] as List<int>)[index];
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset(widget.product['imageUrl'] as String),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: const Color.fromRGBO(245, 247, 249, 1),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    '\$${widget.product['price']}',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: (widget.product['sizes'] as List<int>).length,
+                      itemBuilder: (context, index) {
+                        final size =
+                            (widget.product['sizes'] as List<int>)[index];
 
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selectedSize = size;
-                            });
-                          },
-                          child: Chip(label: Text(size.toString()),
-                          backgroundColor:selectedSize== size? Theme.of(context).colorScheme.primary : null,),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.shopping_cart, color: Colors.black),
-                    label: const Text(
-                      'Add to Cart',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      minimumSize: const Size(double.infinity, 50),
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedSize = size;
+                              });
+                            },
+                            child: Chip(
+                              label: Text(size.toString()),
+                              backgroundColor: selectedSize == size
+                                  ? Theme.of(context).colorScheme.primary
+                                  : null,
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                        color: Colors.black,
+                      ),
+                      label: const Text(
+                        'Add to Cart',
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
